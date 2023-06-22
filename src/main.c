4aed1520e829937f5dc6593ef8e62ad23ec99353
Stephen Jones <stephenj99642@gmail.com>
2023-06-22 01:27:32 +0100

    Add in some memory

--- src/main.c
+++ src/main.c
@@ -1,14 +1,52 @@
 #include "ARMTinyVM.h"
 
+// FUNCTION DECLARATIONS
+int main();
+uint8_t readByte(uint32_t addr);
+void writeByte(uint32_t addr, uint8_t value);
+void softwareInterrupt(uint8_t number);
+
+
+// Static variables
+#define MEM_SIZE 1024
+#define HIGHEST_ADDRESS (MEM_SIZE-1)
+uint8_t memory[MEM_SIZE];
+
+
+// FUNCTION DEFINITIONS
 
 int main()
 {
     VM_interaction_instructions instrs = {
-        .readByte = NULL,
-        .writeByte = NULL,
-        .softwareInterrupt = NULL
+        .readByte = readByte,
+        .writeByte = writeByte,
+        .softwareInterrupt = softwareInterrupt
     };
-    VM_instance vm = VM_new(&instrs, 0xFFFE, 0);
+    VM_instance vm = VM_new(&instrs, HIGHEST_ADDRESS, 0);
 
     return 0;
+}
+
+
+uint8_t readByte(uint32_t addr)
+{
+    if (addr < MEM_SIZE) {
+        return memory[addr];
+    } else {
+        return 0xFF;
+    }
+}
+
+
+void writeByte(uint32_t addr, uint8_t value)
+{
+    if (addr < MEM_SIZE) {
+        memory[addr] = value;
+    }
+}
+
+
+void softwareInterrupt(uint8_t number)
+{
+
 }
